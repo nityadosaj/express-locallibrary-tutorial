394ea579c27f8f91a324ef8cf32273783e4913a7
Nitya Dosaj <nitya.dosaj@gmail.com>
2021-11-17 14:06:02 -0800

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://admin:nitya@cluster0.wn0ai.mongodb.net/nitya_dosaj?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://admin:nitya@cluster0.wn0ai.mongodb.net/nitya_dosaj?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
